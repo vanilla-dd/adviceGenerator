8129d5f4bb1bade6091a23e283ca6f5e3a33e690
whiteHawk <freeidlll6@gmail.com>
2023-06-22 13:13:56 +0530

    improved code lil bit

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ const adviceId = document.querySelector(".advice__id");
 const advicegen = document.querySelector(".advice__gen");
 const getAdvice = async () => {
   try {
-    const response = await fetch("https://api.adviceslip.com/advice");
-    const advice = await response.json();
-    adviceWrapper.textContent = `"${advice.slip.advice}"`;
-    adviceId.innerText = advice.slip.id;
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    const { advice, id } = data.slip;
+    adviceWrapper.textContent = `"${advice}"`;
+    adviceId.innerText = id;
   } catch (error) {
     console.log(error);
     alert("api not working");
