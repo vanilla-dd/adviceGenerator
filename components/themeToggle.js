c80059e19051589e43a1df9c79d1f6140898e4ef
whiteHawk <freeidlll6@gmail.com>
2023-06-27 22:41:49 +0530

    added local storage for theme

--- components/themeToggle.js
+++ components/themeToggle.js
@@ -1,10 +1,24 @@
 const themeSwitcher = () => {
   const toggleBtn = document.querySelector(".theme__toggle");
-  function toggleTheme() {
-    document.body.classList.toggle("light");
-    toggleBtn.firstElementChild.classList.toggle("hide");
-    toggleBtn.lastElementChild.classList.toggle("hide");
+  const theme = localStorage.getItem("mode");
+  // Helper function to update the toggle button
+  function updateToggleBtn(isLight) {
+    toggleBtn.firstElementChild.classList.toggle("hide", isLight);
+    toggleBtn.lastElementChild.classList.toggle("hide", !isLight);
   }
+
+  function toggleTheme() {
+    const isLight = document.body.classList.toggle("light");
+    localStorage.setItem("mode", isLight ? "light" : null);
+    updateToggleBtn(isLight);
+  }
+
+  // Apply the stored theme on page load
+  if (theme === "light") {
+    document.body.classList.add("light");
+    updateToggleBtn(true);
+  }
+
   toggleBtn.addEventListener("click", toggleTheme);
 };
 export default themeSwitcher;
